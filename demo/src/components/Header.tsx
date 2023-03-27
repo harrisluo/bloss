68fab22e312f2c452b2108a1dd323cc348e8c447
harrisluo <harrishcluo@gmail.com>
2023-03-27 11:03:53 -0500

    Update bloss-js

--- demo/src/components/Header.tsx
+++ demo/src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export const Header = () => {
     return <Link to="/">
         <div className="w-full">
-            <h1 className="text-emerald-200 opacity-40 hover:opacity-60 text-lg text-center p-2 border-b border-emerald-200">PGP-25519</h1>
+            <h1 className="text-emerald-200 opacity-40 hover:opacity-60 text-lg text-center p-2 border-b border-emerald-200">bloss-demo</h1>
         </div>
     </Link>
 };
